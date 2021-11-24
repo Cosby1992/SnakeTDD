021f4c14f1ed8639d69a9cd523664c0a105d69f3
cosby1992 <andersbirkedahl@hotmail.com>
2021-11-24 12:25:21 +0100

    Initial setup, added logo

---
+++ README.md
@@ -0,0 +1,25 @@
+# Test Assignment 4
+
+## Mockito Powerups
+
+## Snake Game TDD
+### Environment
+| Name            | Version       |
+|-----------------|--------------:|
+| Java            | 17            |
+| JavaFX (openFX) | 17            |
+| Junit           | 5             | 
+| TestFX          | 4             |
+
+TestFX is a framework for Simple and clean testing of JavaFX Applications. 
+If you want to know more, you can read about it here: 
+[TestFX GitHub Repository](https://github.com/TestFX/TestFX) 
+
+All dependencies are handled with maven and can be found in the pom.xml file in 
+the root folder of this project. 
+
+### Task
+Create a snake game using TDD (Test-Driven-Development), it should include a
+coverage report, mutation testing and static analysis. 
+
+### Solution
--- src/main/java/dk/cosby/games/snaketdd/Game.java
+++ src/main/java/dk/cosby/games/snaketdd/Game.java
@@ -3,16 +3,23 @@ package dk.cosby.games.snaketdd;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Game extends Application {
+
+    //"file:resources/dk/cosby/games/snaketdd/images/snake-logo.png"
+    private final Image LOGO = new Image(Objects.requireNonNull(getClass().getResource("images/snake-logo.png")).toString());
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Game.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("Hello!");
+        stage.setTitle("Snake");
+        stage.getIcons().add(LOGO);
         stage.setScene(scene);
         stage.show();
     }
@@ -20,4 +27,5 @@ public class Game extends Application {
     public static void main(String[] args) {
         launch();
     }
+
 }
