ba386a19d735c93ec0ff753662d6bb107dbd4a93
cosby1992 <andersbirkedahl@hotmail.com>
2021-11-26 13:48:22 +0100

    Test passed

--- src/main/java/dk/cosby/games/snaketdd/Snake.java
+++ src/main/java/dk/cosby/games/snaketdd/Snake.java
@@ -12,11 +12,13 @@ import static dk.cosby.games.snaketdd.GameConstants.TILE_WIDTH;
 public class Snake {
 
     private Queue<Rectangle> snake = new LinkedList<>();
+    private Group snakeGroup = new Group();
+
+    public Snake() {
+        snakeGroup.setId("snake");
+    }
 
     public Group createNewSnake() {
-
-        Group snakeGroup = new Group();
-        snakeGroup.setId("snake");
 
         for (int i = 0; i < 3; i++) {
             Rectangle rect = new Rectangle(i * TILE_WIDTH,0, TILE_WIDTH, TILE_HEIGHT);
@@ -31,5 +33,15 @@ public class Snake {
     }
 
 
+    public Group getSnake() {
+        return snakeGroup;
+    }
 
+    public void move(char r) {
+
+        Rectangle snakeEnd = snake.poll();
+        snakeEnd.setX(snakeEnd.getX() + (3 * TILE_WIDTH));
+        snake.add(snakeEnd);
+
+    }
 }
